ca626a9b6fa6623cfd61d5973245e36be901bc63
David Marioni <david@marioni.com>
2011-12-22 10:11:39 +0100

    UIImage Class extension allows scaling and image rotation easyier

---
+++ UIImage-Extensions.h
@@ -0,0 +1,19 @@
+//
+//  UIImage-Extensions.h
+//  LapTimer
+//
+//  Created by David Marioni on 21.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage (CS_Extensions)
+- (UIImage *)imageAtRect:(CGRect)rect;
+- (UIImage *)imageByScalingProportionallyToMinimumSize:(CGSize)targetSize;
+- (UIImage *)imageByScalingProportionallyToSize:(CGSize)targetSize;
+- (UIImage *)imageByScalingToSize:(CGSize)targetSize;
+- (UIImage *)imageRotatedByRadians:(CGFloat)radians;
+- (UIImage *)imageRotatedByDegrees:(CGFloat)degrees;
+
+@end;
